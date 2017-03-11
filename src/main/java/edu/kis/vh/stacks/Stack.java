354c0ca98a84c5dc6ab905bb245fe131a525da02
MichalKubik <michal.kubik27@gmail.com>
2017-03-11 16:46:33 +0100

    zad 3.2

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -1,5 +1,10 @@
 package edu.kis.vh.stacks;
 
+/**
+ * Stack class implementation.
+ * @author Michał Kubik
+ *
+ */
 public class Stack {
 
 	private static final int INT = -1;
@@ -9,29 +14,57 @@ public class Stack {
 
 	private int total = INT;
 	
+	/**
+	 * Returns total amount of items in stack.
+	 * @return amount of items in stack.
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/**
+	 * Adds new item to the stack.
+	 * @param i item to add.
+	 */
 	public void push(int i) {
 		if (!isFull())
 		items[++total] = i;
 	}
 	
+	/**
+	 * Returns <code>true</code> if this stack is empty.
+	 * @return <code>true</code> if stack is empty.
+	 */
 	public boolean isEmpty() {
 		return total == INT;
 	}
 		
+	/**
+	 * Returns <code>true</code> if this stack is full.
+	 * @return <code>true</code> if stack is full.
+	 */
 	public boolean isFull() {
 		return total == ITEMS_SIZE - 1;
 	}
 			
+	/**
+	 * Returns latest item on stack. <br>
+	 * If stack is empty, method will return <code> -1 </code>.
+	 * @return <code> -1 </code> if stack is empty; <br>
+	 * 			latest item on stack otherwise.
+	 */
 	public int top() {
 		if (isEmpty())
 			return INT;
 		return items[total];
 	}
 				
+	/**
+	 * Returns and deletes latest item on stack. <br>
+	 * If stack is empty, method will return <code> -1 </code>.
+	 * @return <code> -1 </code> if stack is empty; <br>
+	 * 			latest item on stack otherwise
+	 */
 	public int pop() {
 		if (isEmpty())
 			return INT;
