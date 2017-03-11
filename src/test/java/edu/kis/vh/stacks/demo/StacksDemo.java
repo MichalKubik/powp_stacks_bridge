8c9192ab38667f9c7b9baf9b4c22d7d02f882a92
MichalKubik <michal.kubik27@gmail.com>
2017-03-11 15:21:12 +0100

    zad 1.13

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -11,6 +11,11 @@ class StacksDemo {
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
 		
+		testStacks(factory);
+		
+	}
+
+	private static void testStacks(DefaultStacksFactory factory) {
 		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(),
 				factory.getFIFOStack(), factory.getHanoiStack()};
 		
@@ -30,7 +35,7 @@ class StacksDemo {
 //		19-27
 		System.out.println("total rejected is "
 				+ ((StackHanoi) stacks[3]).reportRejected());
-		
 	}
+
 
 }
