1dff2503ccc8ff841109a4aff5aa77190161f5df
MichalKubik <michal.kubik27@gmail.com>
2017-03-23 14:32:56 +0100

    zad 1.1

---
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -0,0 +1,39 @@
+package edu.kis.vh.stacks;
+
+public class StackArray {
+	private static final int INT = -1;
+	private static final int ITEMS_SIZE = 12;
+
+	private int[] items = new int[ITEMS_SIZE];	
+
+	private int total = INT;
+	
+	public int getTotal() {
+		return total;
+	}
+
+	public void push(int i) {
+		if (!isFull())
+		items[++total] = i;
+	}
+	
+	public boolean isEmpty() {
+		return total == INT;
+	}
+		
+	public boolean isFull() {
+		return total == ITEMS_SIZE - 1;
+	}
+			
+	public int top() {
+		if (isEmpty())
+			return INT;
+		return items[total];
+	}
+				
+	public int pop() {
+		if (isEmpty())
+			return INT;
+		return items[total--];
+	}
+}
